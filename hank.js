b9ba532a18a1ca64bfb053ec7cf7da60becfa407
Hagli <hagli2001@gmail.com>
2021-05-22 09:42:00 +0700

    added function for checking for PagMan, Saburo, and Hank. Added function for daily HnK

--- hank.js
+++ hank.js
@@ -18,12 +18,24 @@ client.on('ready', () => {
     console.log(config.quote);
     client.user.setActivity(config.act_obj, {type : config.act});
     //console.log(client.channels);
+    HNK();
 });
 
 client.on('message', message => {
     if (message.content.startsWith(`${config.prefix}`)||(message.author.id===config.botID)){
         console.log(`${message.author.username}#${message.author.id} says "${message}" in ${message.channel.name}`);//logs the message, only works properly on text messages
     }//makes it to an if so it only records message with the prefix or things the bot said, as per the request of a friend
+    //checks for stuffs in any message
+    let search = /PagMan/i;
+    if(search.test(message.content)){
+        let saburo = client.emojis.cache.find(emoji => emoji.name == 'saburo');
+        message.react(saburo.id);
+    }
+    search = /saburo/i;
+    if(search.test(message.content)){
+        let pagman = client.emojis.cache.find(emoji => emoji.name == 'PagMan');
+        message.react(pagman.id);
+    }
     //splits  command to things that only 'I' can do
     if (message.author.id===config.userID){
         switch(true){//all variation of what would happen at certain prefix calls
@@ -80,8 +92,8 @@ client.on('message', message => {
                 break;
         }
     }
-    //for stuffs everyone can call, but not hank itself, to avoid recursion thorugh ~echo
-    if(message.author.id!==config.botID){
+    //for stuffs everyone can call, but not a bot
+    if(!message.author.bot){
         switch(true){//all variation of what would happen at certain prefix calls
             //~echo {message} -> bot sends a message as {message}
             case (message.content.startsWith(`${config.prefix}echo`)):
@@ -90,16 +102,28 @@ client.on('message', message => {
                 message.channel.send(msg);
                 break;
         }
+        search = /\bhank\b/i;
+        if(search.test(message.content)&&!(message.content.startsWith(`${config.prefix}echo`))){
+            message.channel.send(config.quote1);
+        }
+        search = new RegExp(config.botID,'i');
+        if(search.test(message.content)&&!(message.content.startsWith(`${config.prefix}echo`))){
+            message.channel.send(config.quote1);
+        }
     }
 });
 
 //function to send daily HNK everyday when it's 20:00
-//and also works to ping the bot every 1 minute so it doesnt sleep, funtion somewhat relegated to dont_sleep for better consistency
+//and also works to ping the bot every 1 minute so it doesnt sleep -> funtion somewhat relegated to dont_sleep for better consistency
 function HNK(){
     let curDate = new Date();
-    let time = curDate.getHours();
-    if (time == '20'){//checks the time
-        client.channels.fetch(token.channel_1).then((channel) => channel.send(`Daily Screenshot of Houseki no Kuni #${config.HNK_date_count}`, {files : [`./images/${config.HNK_date_count}${config.HNK_data_type}`]}));//sends the message
+    let hour = curDate.getHours();
+    if (hour == 20){//checks the time
+        client.channels.fetch(token.channel_1)
+            .then((channel) => channel.send(`Daily Screenshot of Houseki no Kuni #${config.HNK_date_count}`, {files : [`./images/${config.HNK_date_count}${config.HNK_data_type}`]})//sends the message normally
+                .then(console.log("Not poiler time"), channel.send(`Daily Screenshot of Houseki no Kuni #${config.HNK_date_count}`, {files : [`./images/SPOILER_${config.HNK_date_count}${config.HNK_data_type}`]})));//sends the message, but in SPOILER
+                //the implementation above will always return an error statement first if the we gon send a SPOILER thingy
+                //read up on promise (maybe) to try and make a better implementation
         //updating json for screenshot number
         config.HNK_date_count = parseInt(config.HNK_date_count)+1;//add 1 to HNK_date_count
         fs.writeFile('./config.json', JSON.stringify(config), error => {
@@ -115,9 +139,10 @@ function HNK(){
 setInterval(HNK,3600000);//plays HNK() every 1 hour
 
 function dont_sleep(){
-    1+1;
+    return 0;
 }
 setInterval(dont_sleep,1000);//plays dont_sleep every second, so the bot doesnt sleep
+
 
 
 client.login(config.tokenID);
